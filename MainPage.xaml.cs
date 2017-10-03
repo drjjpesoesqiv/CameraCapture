46359b76e9209cd349c325b6ea10b0624ab62d88
James Steinmetz <james.j.steinmetz@outlook.com>
2017-10-03 13:39:11 -0700

    tabs to spaces

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -32,47 +32,47 @@ namespace CameraCapture
             this.InitializeComponent();
         }
 
-		private async void CameraCaptureBtn_Click(object sender, RoutedEventArgs e)
-		{
-			// create new camera capture object and set options
-			CameraCaptureUI cameraCapture = new CameraCaptureUI();
-			cameraCapture.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
-			cameraCapture.PhotoSettings.AllowCropping = false;
+        private async void CameraCaptureBtn_Click(object sender, RoutedEventArgs e)
+        {
+            // create new camera capture object and set options
+            CameraCaptureUI cameraCapture = new CameraCaptureUI();
+            cameraCapture.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
+            cameraCapture.PhotoSettings.AllowCropping = false;
 
-			// call capture file sync and specify photo should be captured
-			StorageFile photo = await cameraCapture.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            // call capture file sync and specify photo should be captured
+            StorageFile photo = await cameraCapture.CaptureFileAsync(CameraCaptureUIMode.Photo);
 
-			// exit if photo is null, ie cancelled operation
-			if (photo == null)
-				return;
+            // exit if photo is null, ie cancelled operation
+            if (photo == null)
+                return;
 
-			// get reference to ~/Pictures folder
-			var pictures = await StorageLibrary.GetLibraryAsync(KnownLibraryId.Pictures);
-			StorageFolder picturesFolder = pictures.SaveFolder;
+            // get reference to ~/Pictures folder
+            var pictures = await StorageLibrary.GetLibraryAsync(KnownLibraryId.Pictures);
+            StorageFolder picturesFolder = pictures.SaveFolder;
 
-			// copy photo to pictures directory
-			await photo.CopyAsync(picturesFolder, "ProfilePhoto.jpg", NameCollisionOption.ReplaceExisting);
-			
-			// stream from file and decode bitmap
-			IRandomAccessStream stream = await photo.OpenAsync(FileAccessMode.Read);
-			BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
-			SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+            // copy photo to pictures directory
+            await photo.CopyAsync(picturesFolder, "ProfilePhoto.jpg", NameCollisionOption.ReplaceExisting);
+            
+            // stream from file and decode bitmap
+            IRandomAccessStream stream = await photo.OpenAsync(FileAccessMode.Read);
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+            SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
 
-			// convert image to be readable by image control, BGRA8 format
-			SoftwareBitmap softwareBitmapBGR8 = SoftwareBitmap.Convert(
-				softwareBitmap,
-				BitmapPixelFormat.Bgra8,
-				BitmapAlphaMode.Premultiplied);
+            // convert image to be readable by image control, BGRA8 format
+            SoftwareBitmap softwareBitmapBGR8 = SoftwareBitmap.Convert(
+                softwareBitmap,
+                BitmapPixelFormat.Bgra8,
+                BitmapAlphaMode.Premultiplied);
 
-			// assign software bitmap to bitmap source
-			SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
-			await bitmapSource.SetBitmapAsync(softwareBitmapBGR8);
+            // assign software bitmap to bitmap source
+            SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
+            await bitmapSource.SetBitmapAsync(softwareBitmapBGR8);
 
-			// set bitmap source as image control source
-			ImageControl.Source = bitmapSource;
+            // set bitmap source as image control source
+            ImageControl.Source = bitmapSource;
 
-			// delete storage file object
-			await photo.DeleteAsync();
-		}
-	}
+            // delete storage file object
+            await photo.DeleteAsync();
+        }
+    }
 }
