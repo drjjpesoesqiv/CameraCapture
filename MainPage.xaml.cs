34044a8938e00505a4cbe4e6c1b1c67823fa48c0
James Steinmetz <james.j.steinmetz@outlook.com>
2017-10-03 13:31:03 -0700

    display captured image in control

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -11,9 +11,12 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 using Windows.Media.Capture;
 using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.Graphics.Imaging;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -31,19 +34,44 @@ namespace CameraCapture
 
 		private async void CameraCaptureBtn_Click(object sender, RoutedEventArgs e)
 		{
+			// create new camera capture object and set options
 			CameraCaptureUI cameraCapture = new CameraCaptureUI();
 			cameraCapture.PhotoSettings.Format = CameraCaptureUIPhotoFormat.Jpeg;
-			cameraCapture.PhotoSettings.CroppedSizeInPixels = new Size(512, 512);
+			cameraCapture.PhotoSettings.AllowCropping = false;
 
+			// call capture file sync and specify photo should be captured
 			StorageFile photo = await cameraCapture.CaptureFileAsync(CameraCaptureUIMode.Photo);
 
+			// exit if photo is null, ie cancelled operation
 			if (photo == null)
 				return;
 
+			// get reference to ~/Pictures folder
 			var pictures = await StorageLibrary.GetLibraryAsync(KnownLibraryId.Pictures);
 			StorageFolder picturesFolder = pictures.SaveFolder;
 
+			// copy photo to pictures directory
 			await photo.CopyAsync(picturesFolder, "ProfilePhoto.jpg", NameCollisionOption.ReplaceExisting);
+			
+			// stream from file and decode bitmap
+			IRandomAccessStream stream = await photo.OpenAsync(FileAccessMode.Read);
+			BitmapDecoder decoder = await BitmapDecoder.CreateAsync(stream);
+			SoftwareBitmap softwareBitmap = await decoder.GetSoftwareBitmapAsync();
+
+			// convert image to be readable by image control, BGRA8 format
+			SoftwareBitmap softwareBitmapBGR8 = SoftwareBitmap.Convert(
+				softwareBitmap,
+				BitmapPixelFormat.Bgra8,
+				BitmapAlphaMode.Premultiplied);
+
+			// assign software bitmap to bitmap source
+			SoftwareBitmapSource bitmapSource = new SoftwareBitmapSource();
+			await bitmapSource.SetBitmapAsync(softwareBitmapBGR8);
+
+			// set bitmap source as image control source
+			ImageControl.Source = bitmapSource;
+
+			// delete storage file object
 			await photo.DeleteAsync();
 		}
 	}
